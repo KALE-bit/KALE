2ad5004255e8edaa76c7286d57d520e53dd1598b
Monet <53163583+KALE-bit@users.noreply.github.com>
2023-03-01 13:43:45 +0800

    completed MNIST part

--- 智能车视觉组/2023W/Deep-Learning/MNIST-training/MNIST-training.py
+++ 智能车视觉组/2023W/Deep-Learning/MNIST-training/MNIST-training.py
@@ -1,139 +1,129 @@
 import torch
-import torchvision
-from torch.utils.data import DataLoader
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import matplotlib.pyplot as plt
-import time
- 
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
 
-batch_size_train = 64
-batch_size_test = 1000
-learning_rate = 0.01
-momentum = 0.5
-log_interval = 10
-random_seed = 1
-torch.manual_seed(random_seed)
- 
-train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./data/', train=True, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_train, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./data/', train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_test, shuffle=True)
- 
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-
-
-
-def show(num, _sum, loss):
-    barLen = 30  
-    perFin = num/_sum
-    numFin = round(barLen*perFin)
-    numNon = barLen-numFin
-    print(
-        f"{num:0>{len(str(_sum))}}/{_sum}",
-        f"|{'█'*numFin}{' '*numNon}|",
-        f"{perFin*100:.2f}%",
-        f"loss:{loss:.4}",
-        end='\r\n'
-    )
-    if num == _sum:
-        print()
-
-
+# 定义神经网络
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
- 
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc2 = nn.Linear(128, 10)
+
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 1024)
+        x = nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
- 
-network = Net()
-optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
- 
-train_losses = []
-train_counter = []
-test_losses = []
-test_counter = [i * len(train_loader.dataset) for i in range(11)]
- 
- 
-def train(epoch):
-    network.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+        return x
+
+# 定义训练和测试函数
+def train(net, device, train_loader, criterion, optimizer, epoch):
+    net.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    for batch_idx, (data, labels) in enumerate(train_loader):
+        data, labels = data.to(device), labels.to(device)
         optimizer.zero_grad()
-        output = network(data)
-        loss = F.nll_loss(output, target)
+        outputs = net(data)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        if batch_idx % log_interval == 0:
-            show(batch_idx*64,len(train_loader.dataset),loss.item())
-            train_losses.append(loss.item())
-            train_counter.append((batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset)))
-            torch.save(network.state_dict(), './model.pth')
-            torch.save(optimizer.state_dict(), './optimizer.pth')
- 
-def test(i):
-    network.eval()
-    test_loss = 0
+
+        # 计算准确率和损失
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        train_loss = running_loss / (batch_idx + 1)
+        train_accuracy = 100.0 * correct / total
+
+        # 输出进度条
+        progress_bar.set_postfix(loss=train_loss, accuracy=train_accuracy)
+        progress_bar.update(1)
+
+        # 检查是否达到目标准确率
+        if train_accuracy >= 98.0:
+            flag = True
+            break
+        else:
+            flag = False
+
+    return flag
+
+def test(net, device, test_loader):
+    net.eval()
+    test_loss = 0.0
     correct = 0
+    total = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            output = network(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.dataset)
-    test_losses.append(test_loss)
-    print('\nTrian :',i)
-    print('Test set: Avg. loss: {:.4f}, Acc. : {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
- 
- 
-train(1)
-test(0)  
-print('test loop\n')
- 
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
-with torch.no_grad():
-    output = network(example_data)
+        for data, labels in test_loader:
+            data, labels = data.to(device), labels.to(device)
+            outputs = net(data)
+            loss = criterion(outputs, labels)
 
- 
-continued_network = Net()
-continued_optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
- 
-network_state_dict = torch.load('model.pth')
-continued_network.load_state_dict(network_state_dict)
-optimizer_state_dict = torch.load('optimizer.pth')
-continued_optimizer.load_state_dict(optimizer_state_dict)
- 
+            # 计算准确率和损失
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            test_loss /= len(test_loader)
+            test_accuracy = 100.0 * correct / total
+            # 输出进度条
+            progress_bar.set_postfix(test_loss=test_loss, test_accuracy=test_accuracy)
+            progress_bar.update(1)
 
-for i in range(1,11):
-    test_counter.append(i*len(train_loader.dataset))
-    train(i)
-    test(i)
+        return test_accuracy
+
+
+# 定义超参数
+batch_size = 128
+learning_rate = 0.01
+num_epochs = 20
+# 加载数据集
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+# 初始化神经网络、损失函数和优化器
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+net = Net().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+
+# 定义进度条
+train_iter = len(train_loader.dataset) // batch_size
+test_iter = len(test_loader.dataset) // batch_size
+progress_bar = tqdm(total=train_iter * num_epochs + test_iter)
+
+def output():
+    # 训练神经网络
+    flag = False
+    for epoch in range(num_epochs):
+        train_flag = train(net, device, train_loader, criterion, optimizer, epoch)
+        if train_flag:
+            flag = True
+            break
+        else:
+            flag = False
+
+    # 测试神经网络
+    test_accuracy = test(net, device, test_loader)
+    print('Test Accuracy: %.2f %%' % (test_accuracy))
+
+    # 检查是否达到目标准确率
+    if flag and test_accuracy >= 98.0:
+        print('Achieved target accuracy of 98% on test set!')
+    else:
+        output()
+
+output()
